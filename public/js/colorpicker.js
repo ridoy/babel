68577b3cbbdd42e608da25a4f4394872220bac33
ridoy <majumdar.ridoy@gmail.com>
2022-05-22 16:01:02 -0400

    Consolidate stylesheets

--- public/js/colorpicker.js
+++ public/js/colorpicker.js
@@ -1,16 +1,27 @@
-let defaultColor = "#ecf4f9";
+// Background color selection widget
+
+const body = document.querySelector("body");
+const returnToDefaultButton = document.querySelector("#background-color-select a");
+let defaultColor = "#ecf4f9"; // pale blue
 let selectedColor = getCookie("bgcolor");
-if (selectedColor) document.querySelector("body").style.background = selectedColor;
+
+if (selectedColor) {
+  body.style.background = selectedColor;
+}
+
 colorPicker = document.querySelector("#background-color-select input");
 colorPicker.value = (selectedColor) ? selectedColor : defaultColor;
+
 colorPicker.addEventListener("input", updateColor, false);
-document.querySelector("#background-color-select a").addEventListener("click", returnToDefault, false);
+returnToDefaultButton.addEventListener("click", returnToDefault, false);
 colorPicker.select();
+
 function updateColor(event) {
   setCookie("bgcolor", event.target.value, 60);
-  document.querySelector("body").style.background = event.target.value;
+  body.style.background = event.target.value;
 }
 function returnToDefault() {
   setCookie("bgcolor", defaultColor, 60);
-  document.querySelector("body").style.background = defaultColor;
+  body.style.background = defaultColor;
+  colorPicker.value = defaultColor;
 }
